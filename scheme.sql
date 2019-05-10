ceb61d6fee4717ed5792560d81a3b114c59422a2
sip615 <sunilpatel.328@gmail.com>
2019-05-10 17:36:58 -0400

    another final pass

--- scheme.sql
+++ scheme.sql
@@ -19,3 +19,22 @@ CREATE TABLE `products`
   PRIMARY KEY
 (`item_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--Get everything from the products table--
+SELECT *
+FROM products;
+
+--Populate the table--
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("Uncharted 4", "Video Games", 49.95, 150),
+    ("DOOM", "Video Games", 59.99, 200),
+    ("Crate of Spam", "Food and Drink", 24.50, 50),
+    ("Cool Shades", "Apparel", 75.00, 5),
+    ("Worn Denim Jeans", "Apparel", 54.25, 35),
+    ("Survival Towel", "Necessities", 42.42, 42),
+    ("Bill and Ted's Excellent Adventure", "Films", 15.00, 25),
+    ("Mad Max: Fury Road", "Films", 25.50, 57),
+    ("Monopoly", "Board Games", 30.50, 35),
+    ("Yahtzee", "Board Games", 19.95, 23);
